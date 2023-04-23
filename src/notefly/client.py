8ba57e742c955492db5b868ff06384289cfa6179
Steven Almeroth <fedor@primesite.dev>
2023-04-23 09:51:37 -0400

    pub/sub routes

--- src/notefly/client.py
+++ src/notefly/client.py
@@ -4,9 +4,25 @@ from quart import Quart, render_template
 
 app = Quart(__name__)
 
-@app.get('/')
-async def index():
-    return await render_template("client.html")
+@app.route('/')
+def index():
+    return """
+    <html>
+    <head></head>
+    <body>
+    <a href="/pub">/pub</a></br>
+    <a href="/sub">/sub</a>
+    </body>
+    </html>
+    """
+
+@app.get('/pub')
+async def pub():
+    return await render_template("publish.html")
+
+@app.get('/sub')
+async def sub():
+    return await render_template("subscribe.html")
 
 def run() -> None:
     app.run(port=5000)
