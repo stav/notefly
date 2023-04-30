49722c1a8d9cda9b477feb2b573477895c4c8db9
Steven Almeroth <fedor@primesite.dev>
2023-04-30 19:27:25 -0400

    client

---
+++ src/notefly/client.py
@@ -0,0 +1,30 @@
+from quart import Quart, render_template
+
+app = Quart(__name__)
+
+
+@app.route('/')
+def index():
+    return """
+    <html>
+    <head></head>
+    <body>
+    <a href="/pub">/pub</a></br>
+    <a href="/sub">/sub</a>
+    </body>
+    </html>
+    """
+
+
+@app.get('/pub')
+async def pub():
+    return await render_template("publish.html")
+
+
+@app.get('/sub')
+async def sub():
+    return await render_template("subscribe.html")
+
+
+def run() -> None:
+    app.run(port=5000)
